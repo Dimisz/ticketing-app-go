8c103f917d29793e019a46b4917d7129afdfdf30
Vladimir Solovyov <leilyngale@icloud.com>
2024-11-02 20:36:27 +0800

    add json utils

--- backend/cmd/api/handlers.go
+++ backend/cmd/api/handlers.go
@@ -1,26 +1,16 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
 	"net/http"
 )
 
 func (app *application) AllMovies(w http.ResponseWriter, r *http.Request) {
 	movies, err := app.DB.AllMovies()
 	if err != nil {
-		fmt.Println(err)
+		app.ErrorJSON(w, err)
 		return
 	}
-
-	out, err := json.Marshal(movies)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.WriteJSON(w, http.StatusOK, movies)
 }
 
 func (app *application) Home(w http.ResponseWriter, r *http.Request) {
@@ -33,13 +23,5 @@ func (app *application) Home(w http.ResponseWriter, r *http.Request) {
 		Message: "Go Movies up an running",
 		Version: "1.0.0",
 	}
-
-	out, err := json.Marshal(payload)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	w.Header().Set("Content-Type", "application/json")
-	w.WriteHeader(http.StatusOK)
-	w.Write(out)
+	_ = app.WriteJSON(w, http.StatusOK, payload)
 }
