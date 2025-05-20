afe217029489e8c3ec0539f70db760354d5b5b41
Vladimir Solovyov <leilyngale@icloud.com>
2025-05-20 15:23:05 +0800

    Scaffold the backend app

--- backend/cmd/api/main.go
+++ backend/cmd/api/main.go
@@ -1,64 +1,30 @@
 package main
 
 import (
-	"backend/internal/repository"
-	"backend/internal/repository/dbrepo"
-	"flag"
 	"fmt"
 	"log"
 	"net/http"
-	"time"
 )
 
 const PORT = 8080
 
 type application struct {
-	DSN          string
-	Domain       string
-	DB           repository.DatabaseRepo
-	auth         Auth
-	JWTSecret    string
-	JWTIssuer    string
-	JWTAudience  string
-	CookieDomain string
+	Domain string
 }
 
 func main() {
 	// set application config
 	var app application
-	// read from command line
-	flag.StringVar(&app.DSN, "dsn", "host=localhost port=5432 user=postgres password=postgres dbname=movies sslmode=disable timezone=UTC connect_timeout=5", "postgres connection string")
-	flag.StringVar(&app.JWTSecret, "jwt-secret", "verysecret", "signing secret")
-	flag.StringVar(&app.JWTIssuer, "jwt-issuer", "example.com", "signing issuer")
-	flag.StringVar(&app.JWTAudience, "jwt-audience", "example.com", "signing audience")
-	flag.StringVar(&app.CookieDomain, "cookie-domain", "localhost", "cookie domain")
-	flag.StringVar(&app.Domain, "domain", "example.com", "domain")
-	flag.Parse()
-	// connect to a db
-	conn, err := app.connectToDb()
-	if err != nil {
-		log.Fatal(err)
-	}
-	app.DB = &dbrepo.PostgresDBRepo{DB: conn}
-	defer app.DB.Connection().Close()
+	app.Domain = "example.com"
+	// read from comand line
 
-	app.auth = Auth{
-		Issuer:        app.JWTIssuer,
-		Audience:      app.JWTAudience,
-		Secret:        app.JWTSecret,
-		TokenExpiry:   time.Minute * 15,
-		RefreshExpiry: time.Hour * 24,
-		CookiePath:    "/",
-		CookieName:    "refresh_token",
-		CookieDomain:  app.CookieDomain,
-	}
+	// connect to the database
+
 	// start a web server
-	log.Printf("starting application on port %d...", PORT)
-
-	err = http.ListenAndServe(fmt.Sprintf(":%d", PORT), app.routes())
+	log.Println("starting appliation on port", PORT)
+	http.HandleFunc("/", Hello)
+	err := http.ListenAndServe(fmt.Sprintf(":%d", PORT), nil)
 	if err != nil {
 		log.Fatal(err)
 	}
 }
-
-//go get -u github.com/go-chi/chi/v5
